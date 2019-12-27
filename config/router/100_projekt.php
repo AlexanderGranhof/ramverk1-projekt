9bf2b1376e22a7d10bcd0fb1b3dcc779e549d799
Alexander Granhof <alexander.g737@gmail.com>
2019-12-27 15:34:51 +0100

    Added new controllers to router config

---
+++ config/router/100_projekt.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Load the stylechooser as a controller class.
+ */
+return [
+    "routes" => [
+        [
+            "info" => "Style chooser.",
+            "mount" => "posts",
+            "handler" => "\Algn\Controller\PostsController",
+        ],
+        [
+            "mount" => "user",
+            "handler" => "\Algn\Controller\UserController"
+        ]
+    ]
+];
