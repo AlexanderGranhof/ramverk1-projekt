43ed20806fe0dd39586d4a850e38befb37e5557f
Alexander Granhof <alexander.g737@gmail.com>
2019-12-27 15:35:03 +0100

    Added routes to navbar

---
+++ config/navbar/plain.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Supply the basis for the navbar as an array.
+ */
+return [
+    // Use for styling the menu
+    "class" => "my-navbar",
+ 
+    // Here comes the menu items/structure
+    "items" => [
+        [
+            "text" => "Hem",
+            "url" => "",
+            "title" => "Första sidan, börja här.",
+        ],
+        [
+            "text" => "Posts",
+            "url" => "posts",
+            "title" => ""
+        ],
+        [
+            "text" => "User",
+            "url" => "user",
+            "title" => ""
+        ]
+    ],
+];
