2cc924e0d8a7493c64e7b796cada45913fcb7726
Alexander Granhof <alexander.g737@gmail.com>
2020-01-04 17:33:47 +0100

    Added data to home index view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -30,8 +30,29 @@ class HomeController implements ContainerInjectableInterface
     public function indexAction(): object {
         $page = $this->di->get("page");
 
+        $post = new Post();
+        $user = new User();
+
+
+        $tags = $post->popularTags();
+        $comments = $post->topComments();
+        $posts = $post->top();
+
+        $users = $user->all();;
+
+        foreach($users as &$row) {
+            $row["score"] = $user->score($row["id"]);
+        }
+
+        usort($users, function($a, $b) {
+            return $b['score'] - $a['score'];
+        });
+
         $page->add("algn/home/index", [
-            
+            "tags" => $tags,
+            "comments" => $comments,
+            "users" => $users,
+            "posts" => $posts
         ]);
 
         return $page->render();
