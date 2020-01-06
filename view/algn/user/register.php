80ea1f04f8ca281c0183ee49c38e7c6d2c71c4bc
Alexander Granhof <alexander.g737@gmail.com>
2020-01-06 21:57:22 +0100

    Added container

--- view/algn/user/register.php
+++ view/algn/user/register.php
@@ -1,17 +1,18 @@
-<h1>Register</h1>
-
-<form action="" method="POST">
-    <div class="input-container">
-        <label for="username">Username</label>
-        <input required type="text" name="username" id="username">
-    </div>
-    <div class="input-container">
-        <label for="password">Password</label>
-        <input required type="password" name="password" id="password">
-    </div>
-    <div class="input-container">
-        <label for="email">Email</label>
-        <input required type="email" name="email" id="email">
-    </div>
-    <input type="submit" value="Sign up">
-</form>
+<div class="login-container">
+    <h1>Register</h1>
+    <form action="" method="POST">
+        <div class="input-container">
+            <label for="username">Username</label>
+            <input required type="text" name="username" id="username">
+        </div>
+        <div class="input-container">
+            <label for="password">Password</label>
+            <input required type="password" name="password" id="password">
+        </div>
+        <div class="input-container">
+            <label for="email">Email</label>
+            <input required type="email" name="email" id="email">
+        </div>
+        <input type="submit" value="Sign up">
+    </form>
+</div>
