98ce1a75544cc7413ef34bd367aee1ef142e5fd4
Alexander Granhof <alexander.g737@gmail.com>
2020-01-06 21:56:52 +0100

    Use background-image instead of img

--- view/algn/home/index.php
+++ view/algn/home/index.php
@@ -1,13 +1,16 @@
 <div class="container">
-    <img src="img/home-fruit.png" alt="">
-    <div>
+    <!-- <img src="img/home-fruit.png" alt=""> -->
+    <div></div>
+    <div class="content-container">
         <div class="title-container">
             <h1 class="title"><span>Food</span>Flow</h1>
             <h4 class="slogan">The best cooking community online</h4>
         </div>
-        <div class="call-to-action">
-            <span>Browse posts</span>
-        </div>
+        <a href="posts">
+            <div class="call-to-action">
+                <span>Browse posts</span>
+            </div>
+        </a>
         <span class="footer">by foodies for foodies</span>
     </div>
 </div>
