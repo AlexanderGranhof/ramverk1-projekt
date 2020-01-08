7193c089db0f39052e79ca316dc9d8b79eab7547
Alexander Granhof <alexander.g737@gmail.com>
2020-01-08 14:31:45 +0100

    If item is empty, then dont display it

--- view/algn/home/popular.php
+++ view/algn/home/popular.php
@@ -43,41 +43,47 @@
     <div style="width: 100%">
         <h1 class="most-popular-title">Most popular post</h1>
         <div class="post">
-            <a href="profile/<?= $post["username"] ?>" class="username"><?= $post["username"] ?> | <?= $post["score"] ?? 0 ?> points | <?= time_elapsed_string_home($post["created"]) ?></a>
-            <a href="posts/<?= $post["id"] ?>">
-                <h1 class="title"><?= $post["title"] ?></h1>
-                <div class="tags">
-                    <?php foreach($postTags as $tag): ?>
-                    <a class="tag-wrapper" href="posts?tags=<?= $tag ?>">
-                        <span class="tag"><?= $tag ?></span>
-                    </a>
-                    <?php endforeach; ?>
-                </div>
-                <div class="content">
-                    <?= $parsedown->text($post["content"]) ?>
-                </div>
-            </a>
+            <?php if ($post): ?>
+                <a href="profile/<?= $post["username"] ?>" class="username"><?= $post["username"] ?> | <?= $post["score"] ?? 0 ?> points | <?= time_elapsed_string_home($post["created"]) ?></a>
+                <a href="posts/<?= $post["id"] ?>">
+                    <h1 class="title"><?= $post["title"] ?></h1>
+                    <div class="tags">
+                        <?php foreach($postTags as $tag): ?>
+                        <a class="tag-wrapper" href="posts?tags=<?= $tag ?>">
+                            <span class="tag"><?= $tag ?></span>
+                        </a>
+                        <?php endforeach; ?>
+                    </div>
+                    <div class="content">
+                        <?= $parsedown->text($post["content"]) ?>
+                    </div>
+                </a>
+            <?endif;?>
         </div>
     </div>
     <div class="user-tag-container">
         <div>
             <h1 class="most-popular-title">Most popular User</h1>
-            <div class="user-description">
-                <img src="<?= $grav_url ?>" alt="">
-                <div class="username-container">
-                    <h1 class="username"><?= $user["username"] ?></h1>
-                    <span class="light">User score: <?= $score ?? 0 ?></span>
+            <? if($user): ?>
+                <div class="user-description">
+                    <img src="<?= $grav_url ?>" alt="">
+                    <div class="username-container">
+                        <h1 class="username"><?= $user["username"] ?></h1>
+                        <span class="light">User score: <?= $score ?? 0 ?></span>
+                    </div>
                 </div>
-            </div>
+            <?php endif ?>
         </div>
         <div>
             <h1 class="most-popular-title" style="text-align: center">Most popular tag</h1>
-            <?php foreach($tags as $tag => $tagScore): ?>
-                <a href="posts?tags=<?= $tag ?>">
-                    <span style="display: grid; place-self: center; width: fit-content; margin: 0 auto" class="tag large"><?= $tag ?>: <?= $tagScore ?></span>
-                </a>
-                <?php break; ?>
-            <?php endforeach; ?>
+            <?php if (count($tags)): ?>
+                <?php foreach($tags as $tag => $tagScore): ?>
+                    <a href="posts?tags=<?= $tag ?>">
+                        <span style="display: grid; place-self: center; width: fit-content; margin: 0 auto" class="tag large"><?= $tag ?>: <?= $tagScore ?></span>
+                    </a>
+                    <?php break; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </div>
     </div>
 </div>
