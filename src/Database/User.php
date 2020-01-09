fd45193a8be5bc6c7ed2a880cd582222fd3fdd99
Alexander Granhof <alexander.g737@gmail.com>
2020-01-09 14:01:55 +0100

    Make the user 'admin' a mod by default

--- src/Database/User.php
+++ src/Database/User.php
@@ -71,9 +71,11 @@ class User extends Database
     public function register($username, $password, $email) {
         $password = hash("sha256", $password);
 
+        $isAdmin = $username == "admin";
+
         try {
-            $stmt = $this->db->prepare("INSERT INTO users (username, password, email) VALUES (:username, :password, :email)");
-            $stmt->execute(["username" => $username, "password" => $password, "email" => $email]);
+            $stmt = $this->db->prepare("INSERT INTO users (username, password, email, moderator) VALUES (:username, :password, :email, :mod)");
+            $stmt->execute(["username" => $username, "password" => $password, "email" => $email, "mod" => $isAdmin ? 1 : 0]);
 
             $stmt = $this->db->prepare("SELECT * FROM users WHERE username = :username AND password = :password AND email = :email");
             $stmt->execute(["username" => $username, "password" => $password, "email" => $email]);
