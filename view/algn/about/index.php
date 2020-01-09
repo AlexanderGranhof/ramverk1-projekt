05f55adab3875ea4cbbe701a6e30ba3bbc508e7d
Alexander Granhof <alexander.g737@gmail.com>
2020-01-09 14:19:44 +0100

    Created an about view

---
+++ view/algn/about/index.php
@@ -0,0 +1,7 @@
+<h1>About</h1>
+
+<p>This webpage is a part of a <a target="_BLANK" href="https://www.bth.se/utbildning/program/pagwe20h/">web developer course</a> at <a target="_BLANK" href="https://www.bth.se/">Blekinge Tekniska Högskola.</a> The goal of this assignment is to create a reddit/stackoverflow clone with our own theme. I chose to use the food theme mixed with the stackoverflow name; FoodFlow.</p>
+<p>The core purpose of this webpage is a question, comments and aswers about cooking and food in general. The webpage is built using PHP and the <a target="_BLANK" href="https://github.com/canax">canax framework</a>, a simplified framework built by the program's teachers.</p>
+
+<h1>About the developer</h1>
+<p>My name is Alexander Granhof, you can find me on <a target="_BLANK" href="https://github.com/AlexanderGranhof">Github</a> and the project repository can also be found on <a target="_BLANK" href="https://github.com/AlexanderGranhof/ramverk1-projekt">Github</a>. Im currently 21 years old of writing this (09-01-20) and in my second year of my web development course at Blekinge Tekniska Högskola.</p>
