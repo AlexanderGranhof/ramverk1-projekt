d3bc87dad79f68eba617171b26efa7d8230c9199
Alexander Granhof <alexander.g737@gmail.com>
2020-01-10 14:26:51 +0100

    Added more text in about me

--- view/algn/about/index.php
+++ view/algn/about/index.php
@@ -1,7 +1,15 @@
-<h1>About</h1>
+<h1>About this website</h1>
 
 <p>This webpage is a part of a <a target="_BLANK" href="https://www.bth.se/utbildning/program/pagwe20h/">web developer course</a> at <a target="_BLANK" href="https://www.bth.se/">Blekinge Tekniska Högskola.</a> The goal of this assignment is to create a reddit/stackoverflow clone with our own theme. I chose to use the food theme mixed with the stackoverflow name; FoodFlow.</p>
-<p>The core purpose of this webpage is a question, comments and aswers about cooking and food in general. The webpage is built using PHP and the <a target="_BLANK" href="https://github.com/canax">canax framework</a>, a simplified framework built by the program's teachers.</p>
-
+<p>The core purpose of this webpage is a question, comments and aswers about cooking and food in general. The webpage is built using PHP and the <a target="_BLANK" href="https://github.com/canax">canax framework</a>, a simplified framework built by the program's teachers. The website is designed using an online free-to-use collobarative design tool called <a href="https://www.figma.com">Figma</a>. The design can be <a href="https://www.figma.com/file/5EEoyFgHCwzHRo3Nc93jUK/Ramverk1">found here</a></p>
+<p>In this project we are tasked with posting everything on <a target="_BLANK" href="https://github.com/AlexanderGranhof/ramverk1-projekt">Github</a> along with a README.md file with instructions on how you can download this project and edit it yourself. I also used Github to manage my development of this site by using their project section to organize which features i need to develop. It also helps me to keep track of where i am currently in the development process. </p>
+<p>Keeping track of what needed to be done and set priorities made this project much easier, more than i actually expected. Keeping track of time and managing which features i should prioritize made this project much less pressuring, but still didnt quite have all the time i wanted to polish everything to perfection.</p>
 <h1>About the developer</h1>
-<p>My name is Alexander Granhof, you can find me on <a target="_BLANK" href="https://github.com/AlexanderGranhof">Github</a> and the project repository can also be found on <a target="_BLANK" href="https://github.com/AlexanderGranhof/ramverk1-projekt">Github</a>. Im currently 21 years old of writing this (09-01-20) and in my second year of my web development course at Blekinge Tekniska Högskola.</p>
+<div class="image-text-combo">
+    <img src="img/me.png" alt="">
+    <div>
+        <p>My name is Alexander Granhof, you can find me on <a target="_BLANK" href="https://github.com/AlexanderGranhof">Github</a> and the project repository can also be found on <a target="_BLANK" href="https://github.com/AlexanderGranhof/ramverk1-projekt">Github</a>. Im currently 21 years old of writing this (09/01/20) and in my second year of my web development course at Blekinge Tekniska Högskola.</p>
+        <p>I'm a Javascript and NodeJS fanatic tasked with working with a home brewed library to come up with not-so-ideal solutions in PHP. I'm a mostly open mineded developer ready to learn new things.</p>
+        <p>However regarding PHP im hoping this is the last time i will be forced to be working in this language. I would like to explore other languages and work with familiar languages to design and develop larger solutions in a group in the future.</p>
+    </div>
+</div>
