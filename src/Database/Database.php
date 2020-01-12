89fa45a3de5151688bf492ef839631eef4cdf6a8
Alexander Granhof <alexander.g737@gmail.com>
2020-01-12 17:50:49 +0100

    Removed student db pw. Requested new pw

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -14,11 +14,6 @@ class Database
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
         ];
 
-        if ($_SERVER["SERVER_NAME"] === "www.student.bth.se") {
-            $this->db = new PDO("mysql:host=blu-ray.student.bth.se;dbname=algn18", "algn18", "HY8r5EbwyKBP", $options);
-        } else {
-            $this->db = new PDO("mysql:host=127.0.0.1;dbname=test", "root", "root", $options);
-        }
-
+        $this->db = new PDO("mysql:host=127.0.0.1;dbname=test", "root", "root", $options);
     }
 }
